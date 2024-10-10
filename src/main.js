e280c2dae0f1dd140967ec6878e7018d0918092a
Jean <JFBimCopinski@gmail.com>
2024-10-10 09:59:59 -0300

    feat: finished && not tested code

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import e from "express"
 import workshop_router from "./routers/workshop_router.js"
 import vehicle_router from "./routers/vehicle_router.js"
+import maintenance_router from "./routers/maintenance_router.js"
 import "dotenv/config"
 import "./db.js"
 
@@ -10,5 +11,6 @@ app.use(e.json())
 
 app.use("/workshop", workshop_router)
 app.use("/vehicle", vehicle_router)
+app.use("/maintenance", maintenance_router)
 
 app.listen(process.env.API_PORT, () => console.log("Servidor conectado com sucesso!"))
