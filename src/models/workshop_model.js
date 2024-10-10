a11743114427136554b6a770c57cdff6bfcf2208
Jean <JFBimCopinski@gmail.com>
2024-10-10 08:56:54 -0300

    feat: finished && not tested code

---
+++ src/models/workshop_model.js
@@ -0,0 +1,24 @@
+import { Schema, model } from "mongoose"
+
+export const workshopSchema = new Schema ({
+    name: {
+        type: Schema.Types.String,
+        required: true,
+    },
+    adress: {
+        type: Schema.Types.String,
+        required: true,
+    },
+    specialties: {
+        type: Schema.Types.String,
+        enum: ["MOTOR", "FREIOS", "SUSPENSÃO", "ESTÉTICA"],
+        required: true,
+    },
+    maintenance: {
+        type: Schema.Types.ObjectId,
+        ref: "Maintenance",
+        required: true,
+    }
+})
+
+const Workshop = model("Workshop", workshopSchema)
