2196684aaae9522cca21267cf4a7d80add1b07f4
Jean <JFBimCopinski@gmail.com>
2024-10-10 09:19:24 -0300

    feat: finished && not tested code

---
+++ src/models/vehicle_model.js
@@ -0,0 +1,30 @@
+import { Schema, model } from "mongoose"
+
+export const workshopSchema = new Schema ({
+    plate: {
+        type: Schema.Types.String,
+        required: true,
+        unique: true,
+        /*Conforme a fonte: "https://www.despachantedok.com.br/placa":
+        Placas de veículos são únicas não podem ser clonadas, por isso o "unique".*/
+    },
+    model: {
+        type: Schema.Types.String,
+        required: true,
+    },
+    year: {
+        type: Schema.Types.Number,
+        required: true,
+    },
+    owner: {
+        type: Schema.Types.String,
+        required: true,
+    },
+    maintenance: {
+        type: Schema.Types.ObjectId,
+        ref: "Maintenance",
+        required: true,
+    }
+})
+
+const Workshop = model("Workshop", workshopSchema)
